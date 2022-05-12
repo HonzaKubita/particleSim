76e37b7da51e071fe7bdb877f59c2ce2cbaf687d
Jan Kubita <honza.kubita@gmail.com>
2022-05-12 11:56:45 +0200

    Program name fix

--- electron/electronApp.js
+++ electron/electronApp.js
@@ -30,7 +30,7 @@ class Main {
 		this.mainWindow = new BrowserWindow({
       width: 1300, 
       height: 800,
-      icon: __dirname + '/trexEngineLogo.png',
+      icon: __dirname + '/particleSimLogo.png',
       webPreferences: {
         nodeIntegration: true
       },
